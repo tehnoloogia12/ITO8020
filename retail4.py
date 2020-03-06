e98c742bbb5f15eda8b1f182250e9d1c01f881eb
tehnoloogia12 <60431429+tehnoloogia12@users.noreply.github.com>
2020-03-06 16:04:10 +0200

    Update and rename retail3.py to retail4.py

--- retail4.py
+++ retail4.py
@@ -1,3 +1,5 @@
+### algne töökäik pärineb https://www.edureka.co/blog/apriori-algorithm/
+### 
 import pandas as pd
 from mlxtend.frequent_patterns import apriori
 from mlxtend.frequent_patterns import association_rules
@@ -41,13 +43,20 @@ basket_sets.drop('POSTAGE', inplace=True, axis=1)
 #df=basket_sets
 #df.to_csv('C:\\Users\\pealik\\Documents\\kodu2\\df3-basket_sets.csv', index=False, header=True)
 #print(df)
+
+# siin on mõõdik LIFT, mille väärtus 1 tähendab, et "If the lift is > 1 
+# that lets us know the degree to which those two occurrences are dependent on one another, 
+# and makes those rules potentially useful for predicting the consequent in future data sets. "
+# allikas: https://en.wikipedia.org/wiki/Lift_(data_mining)
 frequent_itemsets = apriori(basket_sets, min_support=0.07, use_colnames=True)
 rules = association_rules(frequent_itemsets, metric="lift", min_threshold=1)
 
 #print('--- rules.head')
 #print(rules.head())
 #### reeglieid tuleks muuta
-rules[ (rules['lift'] >= 6) &
+#### http://rasbt.github.io/mlxtend/user_guide/frequent_patterns/apriori/
+##rules[ (rules['lift'] >= 6) &
+rules[ (rules['support'] >= 0.02) &
       (rules['confidence'] >= 0.8) ]
 
 #print('--- rules')
